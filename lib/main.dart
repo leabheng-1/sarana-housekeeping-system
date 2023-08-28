f6319140d94a4da386b614fe40024fc26e04c563
leab heng <khtop38@gmail.com>
2023-08-28 18:25:55 +0700

    Updated project

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,24 @@ import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:i_system/screen/dashboard.dart';
 import 'package:roundcheckbox/roundcheckbox.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? token = prefs.getString('token');
+
+  runApp(MyApp(initialToken: token));
 }
 
 class MyApp extends StatelessWidget {
+  final String? initialToken;
+
+  MyApp({this.initialToken});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: initialToken != null ? DashboardPage() : LoginPage(),
     );
   }
 }
